8bd393fa2672f3ee76269a0b67abf970596fd8fa
Gresliebear <lesliemwubbel@gmail.com>
2021-09-18 04:16:15 -0400

    Web3 test
    Next time run solidity code with Web3

---
+++ Front-end/demo/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+// you can a javascript 5 differents or more 
+  
+  const handleSubmit = () => {
+
+    console.log("heello")
+  }
+
+  let Name = "Adil"
+  return (
+    // JSX 
+    <div className="App">
+    <h1 className="title"> Upload Your Claim Medical</h1>
+{/* add css react */}
+    <p> How much is your claim?</p>
+
+  {/* Form basically sends web request to a server
+  Wheres the server ? IP:port */}
+    <form method="POST"> 
+    <input type="text"/> 
+
+   
+    </form>
+    <button onClick={handleSubmit}> Submit Claim</button>
+    
+
+    </div>
+  );
+}
+
+export default App;
--- PythonServer/wsgi.py
+++ PythonServer/wsgi.py
@@ -1,34 +1,67 @@
-from flask import Flask
+from flask import Flask,  request, jsonify, url_for, Blueprint, render_template, send_from_directory, current_app, session
+from web3 import Web3
+from decouple import config
+from ComplieSolToPython import compile
 app = Flask(__name__)
 
-from flask import current_app, flash, jsonify, make_response, redirect, request, url_for
+contract_id, contract_interface = compile()
+API_USERNAME = config('HOME')
+INFURA = config('PROID')
+bytecode = contract_interface['bin']
+abi = contract_interface['abi']
 
-# banking license 
-# WSGI
-# Finanical products
 
+# flask endpoint will be used as "API" we send Web request to trigger contract lock and transactions
 @app.route('/') #flask instance app fun() route
 def index(): #view functions
-
+    print(web3.clientVersion)
     dataDict = { 
         "test":"Hello World"
     }
+    url = 'https://mainnet.infura.io/v3/' + INFURA
+    # Provider Infura
+    w3 = Web3(Web3.HTTPProvider(url))
 
-    passObj = jsonify(dataDict)
-    return passObj
+    print(w3.isConnected())
+    condition = w3.isConnected()
+    latestBlock = str(w3.eth.get_block('latest'))
+    dataDict = { 
+        "test":"Hello World",
+        "Web3 Connect?":condition,
+        "latest block": latestBlock
+    }
 
+    w3 = Web3(Web3.EthereumTesterProvider())
 
-@app.route('/api/hello') #flask instance app fun() route
-def index(): #view functions
-
-    # yearn
-    # https://medium.com/aave/flash-loans-one-month-in-73bde954a239
-    # flashloan 
+    ContractObject = w3.eth.contract(abi=abi, bytecode=bytecode)
+    tx_hash = ContractObject .constructor().transact()
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
     
 
-    dataDict = { 
-        "test":"Hello World"
-    }
+    greeter = w3.eth.contract(
+        address=tx_receipt.contractAddress,
+        abi=abi
+    )
+
+    greeter.functions.greet().call()
+
+    tx_hash = greeter.functions.setGreeting('Nihao').transact()
+    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+    greeter.functions.greet().call()
+# Proxy Contracts
+# Address and ABIs
+# AAVE token 0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9
+    # aave is accsible by user
+        # deposit
+        # withdraw
+        # borrow 
+    # https://www.quicknode.com/guides/defi/how-to-make-a-flash-loan-using-aave
+    # MainEntry Point to Aave
+    # is LendingPool
+    # deposit, borrow, withdraw and repayare only for ERC20
+    # WETHGateway for polygon
+
+    # so to interact go calls actions you must have allowance() to spend funds on behalf of msg.sender for at-least amount for the asset being asst
 
     passObj = jsonify(dataDict)
     return passObj
