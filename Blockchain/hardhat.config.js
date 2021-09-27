77da222ce2c2c3d32d96015bc636ea942d84c63c
Gresliebear <lesliemwubbel@gmail.com>
2021-09-26 23:25:59 -0400

    Small Update Incorrect ReadMe

--- Blockchain/hardhat.config.js
+++ Blockchain/hardhat.config.js
@@ -2,7 +2,18 @@ require("@nomiclabs/hardhat-waffle");
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
+ * Implement muiltiple Solidity Compiler https://hardhat.org/guides/compile-contracts.html
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+      },
+      {
+        version: "0.7.6",
+        settings: {},
+      },
+    ],
+  },
 };
